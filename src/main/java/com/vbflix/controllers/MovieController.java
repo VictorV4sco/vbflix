f000b78c67fdf7e20366c23299e71e60828eff66
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-02-01 00:02:01 -0300

    New version

--- src/main/java/com/vbflix/controllers/MovieController.java
+++ src/main/java/com/vbflix/controllers/MovieController.java
@@ -1,6 +1,7 @@
 package com.vbflix.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,12 @@ import com.vbflix.services.MovieService;
 @RestController
 @RequestMapping(value = "/movies")
 public class MovieController {
-	
+
 	@Autowired
 	private MovieService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public MovieDTO findById(@PathVariable Long id) {	//Criando método para retornar objeto MovieDTO
-		return service.findById(id);
+	public ResponseEntity<MovieDTO> getMovieById(@PathVariable Long id) {
+		return service.findById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 	}
 }
--- src/main/java/com/vbflix/services/MovieService.java
+++ src/main/java/com/vbflix/services/MovieService.java
@@ -1,10 +1,11 @@
 package com.vbflix.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vbflix.dto.MovieDTO;
-import com.vbflix.entities.Movie;
 import com.vbflix.repositories.MovieRepository;
 
 @Service
@@ -13,9 +14,9 @@ public class MovieService {
 	@Autowired
 	private MovieRepository repository;
 	
-	public MovieDTO findById(Long id) {
-		Movie entity = repository.findById(id).get();
-		MovieDTO dto = new MovieDTO(entity);
-		return dto;
+	public Optional<MovieDTO> findById(Long id) {
+		return repository.findById(id)
+				.map(movie -> new MovieDTO(movie.getId(), movie.getTitle(), movie.getDescription(), movie.getCategory(),
+						movie.getDirector(), movie.getMainActor(), movie.getReleaseDate()));
 	}
 }
